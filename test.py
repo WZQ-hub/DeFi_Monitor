f8cef38779b4eb939ce9d7c872424158da2aa993
SUPER_WZQ <super1wzq1@outlook.com>
2025-09-20 03:25:34 +0800

    connect

--- test.py
+++ test.py
@@ -3,17 +3,11 @@ from web3 import Web3
 # Connect to the Ethereum network
 w3 = Web3(Web3.HTTPProvider('https://eth-mainnet.g.alchemy.com/v2/JEJmfRm0uQhy2nfe-Ff0-'))
 
-# 1. 导入 Web3 库
-import os
-
-# 2. 从 Alchemy 获取你的 API URL
-# 最佳实践：建议使用环境变量来存储你的API URL，而不是直接写在代码里。
-# 这里为了方便初学者，我们先直接写入。
-# 替换成你自己的 Alchemy HTTPS URL
-
+a = w3.eth.get_block('latest')
 # 4. 检查是否连接成功
 if w3.is_connected():
     print("🎉 Successfully connected to the Ethereum network!")
+    print(a)
 else:
     print("❌ Failed to connect to the Ethereum network.")
     exit() # 如果连接失败，则退出脚本
