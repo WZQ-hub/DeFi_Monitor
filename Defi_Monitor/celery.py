45e6fd13f102841020cb9a07e08e43c4a4f190ca
SUPER_WZQ <super1wzq1@outlook.com>
2025-10-04 00:43:04 +0800

    整合celery，查询sushiswao_v2

--- Defi_Monitor/celery.py
+++ Defi_Monitor/celery.py
@@ -13,5 +13,6 @@ app = Celery('Defi_Monitor')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
